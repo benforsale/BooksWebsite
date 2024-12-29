723eedcbb43f3faf6202977383299048c8589269
benforsale <khtoanailurophile@gmail.com>
2024-12-29 18:20:05 +0700

    Add files via upload

---
+++ BooksWebsite/BooksWebsite/Website test slide/navbar.js
@@ -0,0 +1,23 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     const toggleButton = document.getElementById("navsidebarCollapse");
+//     const sidebar = document.querySelector(".navsidebar");
+//     const closeButton = document.querySelector(".close-btn");
+//     const links = document.querySelectorAll(".navsidebar ul li a");
+
+//     // Open the sidebar
+//     toggleButton.addEventListener("click", () => {
+//         sidebar.classList.add("active");
+//     });
+
+//     // Close the sidebar
+//     closeButton.addEventListener("click", () => {
+//         sidebar.classList.remove("active");
+//     });
+
+//     // Close sidebar on link click
+//     links.forEach(link => {
+//         link.addEventListener("click", () => {
+//             sidebar.classList.remove("active");
+//         });
+//     });
+// });
